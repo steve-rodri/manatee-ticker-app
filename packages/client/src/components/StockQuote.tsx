593f46dd54ce623dcec686e091d22162a729debf
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-12-04 12:04:34 -0500

    use localized time format

--- packages/client/src/components/StockQuote.tsx
+++ packages/client/src/components/StockQuote.tsx
@@ -1,9 +1,9 @@
 import { Title, Text, Loader, Stack } from "@mantine/core"
 import dayjs from "dayjs"
-import utc from "dayjs/plugin/utc"
+import localizedFormat from "dayjs/plugin/localizedFormat"
 
 import { trpc } from "../utils/trpc"
-dayjs.extend(utc)
+dayjs.extend(localizedFormat)
 
 export const StockQuote = ({ ticker }: { ticker: string }) => {
   const { data, isLoading, isError, error } = trpc.quote.useQuery({
@@ -37,9 +37,7 @@ export const StockQuote = ({ ticker }: { ticker: string }) => {
       <Stack spacing={10} align="center">
         <Title order={3}>Time:</Title>
         <Text fz="lg">
-          {data?.t
-            ? dayjs.utc(data.t * 1000).format("ddd MMM D YYYY, h:mma")
-            : null}
+          {data?.t ? dayjs(data.t * 1000).format("llll") : null}
         </Text>
       </Stack>
     </Stack>
