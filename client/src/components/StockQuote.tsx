ad39a24969a3e5cd863faefc7d18e1da13e1eaa8
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-12-03 21:26:01 -0500

    display time and pct change

--- client/src/components/StockQuote.tsx
+++ client/src/components/StockQuote.tsx
@@ -1,12 +1,53 @@
-import { Text, Loader } from "@mantine/core"
+import { Title, Text, Loader, Stack } from "@mantine/core"
+import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
 
 import { trpc } from "../utils/trpc"
+dayjs.extend(utc)
 
 export const StockQuote = ({ ticker }: { ticker: string }) => {
   const { data, isLoading, isError, error } = trpc.quote.useQuery({
     symbol: ticker,
   })
+  const pctChange = (() => {
+    if (data && data.c && data.pc) {
+      const change = ((data.c - data.pc) / data.pc) * 100
+      const rounded = parseFloat(change.toString()).toFixed(2)
+      return rounded
+    }
+  })()
   if (isLoading) return <Loader />
   if (isError) return <Text>{error.message}</Text>
-  return <Text data-testid="quote">${data?.c}</Text>
+  return (
+    <Stack align="center">
+      <Title order={1}>{ticker}</Title>
+      <Stack spacing={10} align="center">
+        <Title order={3}>Current Price:</Title>
+        <Text fz="lg" c="blue" data-testid="quote">
+          ${data?.c}
+        </Text>
+      </Stack>
+      <Stack spacing={10} align="center">
+        <Title order={3}>Percent Change:</Title>
+        <Text
+          fz="lg"
+          c={
+            pctChange !== undefined && parseFloat(pctChange) > 0
+              ? "green"
+              : "red"
+          }
+        >
+          {`${pctChange}%`}
+        </Text>
+      </Stack>
+      <Stack spacing={10} align="center">
+        <Title order={3}>Time:</Title>
+        <Text fz="lg">
+          {data?.t
+            ? dayjs.utc(data.t * 1000).format("ddd MMM D YYYY, h:mma")
+            : null}
+        </Text>
+      </Stack>
+    </Stack>
+  )
 }
