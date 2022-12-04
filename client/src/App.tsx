a845bab6bc5fcc171aaf4b06f5395687d472da85
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2022-12-03 21:47:08 -0500

    make sure uppercase values are being sent to server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,9 @@ import { useTickerForm } from "./hooks"
 const App = () => {
   const [ticker, setTicker] = useState("AAPL")
   const tickerForm = useTickerForm(ticker)
-  const onSubmit = tickerForm.onSubmit(values => setTicker(values.ticker))
+  const onSubmit = tickerForm.onSubmit(values =>
+    setTicker(values.ticker.toUpperCase())
+  )
   return (
     <Center h="100vh" w="100vw">
       <Stack spacing="lg">
